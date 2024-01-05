aeceba9356b62e96a8434977b644a8f0edbd5a4d
Caio Bruno <caiobruno90@yahoo.com>
2024-01-05 16:25:46 -0300

    Inserindo dados no sistema

--- backend/routes.js
+++ backend/routes.js
@@ -2,10 +2,22 @@ const express = require("express")
 
 const router = express.Router()
 
-const {
-    createMemory
-} = require("./controllers/MemoryController")
+const upload = require("./helpers/upload");
 
-router.post("/", (req, res) => createMemory(req, res))
+const {createMemory} = require("./controllers/MemoryController")
+
+  router.post(
+    "/",
+    upload.single("image"),
+    (req, res, next) => {
+      const image = req.file;
+      if (!image) {
+        return res.status(400).json({ msg: "Por favor, envie um arquivo." });
+      }
+      next();
+    },
+    (req, res) => createMemory(req, res)
+  );
+  
 
 module.exports = router
